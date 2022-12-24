c8d0755eaabcad94e513bbf0b922d2963c816937
Valet78 <valet78@mail.ru>
2022-12-25 01:34:20 +0300

    Выполнение задания 9 семинара (бот игра

--- game.py
+++ game.py
@@ -1,63 +1,116 @@
 from login.act import *     # Token bot
 from random import randint
 from datetime import datetime as dt
-from os import getcwd
+from os import getcwd, path
 from csv import *
 
+current_gamer, all_candy, one_candy, two_candy = 0, 0, 0, 0
 
-def game_117(message):            
+
+def game_117(message):
     textov = f'Я, предлагаю Вам сыграть в игру:\n \
         В игре у игроков есть кучка, в ней 117 конфет. \n \
         Два игрока поочереди достают из кучки конфетки, \n \
         но не более 28 конфет за один ход. \n \
         Выигрывает тот игрок, чей ход окажется последним. \n \
         Он и забирает все конфеты себе.'  
-    bot.send_bot(message, textov)
-    bot.send_bot(message, 'Играем да/нет')  
+    send_bot(message, textov)
+    send_bot(message, 'Играем да/нет')  
     bot.register_next_step_handler(message, start_game)   
 
-def start_game(message) -> str:
-    # send_bot = lambda x: bot.send_message(message.chat.id, x)
+
+def start_game(message): 
+    global current_gamer, all_candy    
     if message.text == 'да':    
         log_win('Начало игры', ValueDate())
         textov = f'Отлично! Сейчас мы определимся, кто будет ходить первым. \n \
                 Игрок 1 - это собственно Вы сами \n \
                 Игрок 2 - это я, бот Firsttest_bot.'    
-        send_bot(message, textov)
-        gamer = cur_gamer(0)
-        send_bot(message, f'Итак, первым ходит Игрок {gamer}')
-        run_game(gamer)
-        # bot.register_next_step_handler(message, choose_game) 
-        msg = ''
+        send_bot(message, textov)        
+        current_gamer = cur_gamer(0)
+        all_candy = 117
+        send_bot(message, f'Итак, первым ходит Игрок {current_gamer}')      
+        run_game(message)       
     else:  
         send_bot(message, "Ну, и ладно!!!")
     
-def send_bot(message, in_text:str):
-    bot.send_message(message.chat.id, in_text)
 
-             
 
+# Основной блок игры
+def run_game(message):
+    global current_gamer, all_candy, one_candy, two_candy       
+    if all_candy != 0:
+        if current_gamer == 2:      # Ход бота
+            resBot = lambda x: x if x <= 28 else randint(1, 28)
+            minus_candy = resBot(all_candy)
+            all_candy -= minus_candy
+            two_candy += minus_candy
+            current_gamer = cur_gamer(current_gamer)         
+            if all_candy == 0: 
+                send_bot(message,'Ой! Кажется, я забрал последние конфеты.')
+                final_game(message)
+            else:
+                send_bot(message, f'Я (бот) забрал {minus_candy} конфет(ы). \n И теперь у меня {two_candy} конфет(ы), а в куче осталось {all_candy}.')
+                # Переход к первому игроку
+                send_bot(message, f'В куче {all_candy} конфет(ы). Сколько заберёте Вы?')
+                bot.register_next_step_handler(message, step_gamer)
+        if current_gamer == 1 and all_candy == 117:      # Ход  игрока
+            send_bot(message, f'В куче {all_candy} конфет(ы). Сколько заберёте Вы?')
+            bot.register_next_step_handler(message, step_gamer) 
+    
+# Финал игры
+def  final_game(message):   
+        global current_gamer, all_candy, one_candy, two_candy
+        current_gamer = cur_gamer(current_gamer)
+        win_gamer = message.from_user.username if current_gamer == 1 else 'Firsttest_bot'
+        textov = f'Итак, игра закончилась победой {win_gamer}. \n Станент скучно, набирай /game_117, сыграем тогда снова.'
+        send_bot(message, textov)
+        current_gamer, all_candy, one_candy, two_candy = 0, 117, 0, 0
+        log_win(f'Конец игры. Победил {win_gamer}.', ValueDate())
+
+# Ход игрока
+def step_gamer(message): 
+    global current_gamer, all_candy, one_candy, two_candy      
+    minus_candy = int(message.text)
+    if minus_candy > 0 and minus_candy < 29:    
+        one_candy += minus_candy
+        all_candy -= minus_candy     
+        current_gamer = cur_gamer(current_gamer)
+        if all_candy == 0: final_game(message)   
+        else:
+            send_bot(message, f'В куче остаётся {all_candy} конфет(ы). \n У Вас теперь {one_candy}. Ход второго игрока.')        
+            run_game(message)
+    else:
+        send_bot(message, f'Вы, наверное, ввели неверное значение. \nВы можете забрать от 1 до 28 конфет за ход.\n \
+            В куче {all_candy} конфет(ы). Сколько заберёте Вы?')
+        bot.register_next_step_handler(message, step_gamer)
+    
+
+# Отправка сообщений в чат
+def send_bot(message, in_text:str):    
+    id_chat = message.chat.id
+    bot.send_message(id_chat, in_text)
+
+# Запись событий в файл
 def log_win(strMassage: str, strTime: str):
     pathFile = getcwd() + '\\log.csv'
-    names = ['Событие', 'Время']
-    try:
-        with open(pathFile, mode = 'a', encoding='utf-8', newline='\r') as file:
-            file_writer = DictWriter(file, lineterminator="\r", fieldnames=names)
-            file_writer.writerow({'Событие': strMassage, 'Время':strTime})
-    except FileNotFoundError:       
-        with open(pathFile, mode = 'w', encoding='utf-8', newline='\r') as file:    
-            file_writer = DictWriter(file, lineterminator="\r", fieldnames=names)
-            file_writer.writeheader()
-            file_writer.writerow({'Событие': strMassage, 'Время':strTime})
+    fil_exist = path.exists(pathFile)
+    names = ['Время', 'Событие']
+    with open(pathFile, mode = 'a', encoding='utf-8', newline='\r') as file_csv:         
+        file_writer = DictWriter(file_csv, lineterminator="\r", fieldnames=names)
+        if  not fil_exist: file_writer.writeheader()         # Если файла не существовало  
+        file_writer.writerow({'Время':strTime, 'Событие': strMassage})
+        
 
+# Формирование текущей даты и времени для файла
 def ValueDate() -> str:
     time = dt.now()   
     res_day = [str(time.day), str(time.month), str(time.year)]
     res_time = [str(time.hour), str(time.minute), str(time.second)]
-    return (".".join(res_day) + ' г. ' + ':'.join(res_time))
+    return (".".join(res_day) + 'г. ' + ':'.join(res_time))
 
-
-def cur_gamer(tg: int) -> int:          # Выбор первого хода
+# Перебор текущего игрока и выбор игрока для первого хода
+def cur_gamer(tg: int) -> int:          
     match tg:
         case 0: return randint(1, 2)
         case 1: return 2
@@ -65,31 +118,5 @@ def cur_gamer(tg: int) -> int:          # Выбор первого хода
     
 
     
-def run_game(current_gamer: int) -> int:
-    one_candy, two_candy = 0, 0   # количество конфет у игроков
-    num_candy = 117
-
-"""    
-    while num_candy != 0:
-        text_mess = 'Ход первого игрока. ' if current_gamer == 1 else 'Ход второго игрока. '
-        Up.message.reply_text(text_mess, end = '')
-        minus_candy = step_bot(num_candy) if type_game == 2 and current_gamer == 2 else step_hum(num_candy)
-        step_game = lambda x, y: True if y <= 28 and y <= x else False
-        num_game = lambda x: 1 if x == 2 else 2
-        if step_game(num_candy, minus_candy): 
-            num_candy -= minus_candy
-            if current_gamer == 1: one_candy += minus_candy
-            else: two_candy += minus_candy 
-            current_gamer = num_game(current_gamer)
-        else: Up.update.message.reply_text('Вы можете одновременно забрать 28 конфет и не более оставшихся.')
-    
-    return num_game(current_gamer)
-
-  
-def step_bot(num_candy: int) -> int:
-    resBot = lambda x: x if x <= 28 else randint(1, 28)
-    min_candy = resBot(num_candy)
-    Up.message.reply_text(f'Второй игрок (бот) забрал {min_candy} из {num_candy}')
-    return min_candy
-    
-    """
+if __name__ == '__main__':
+    log_win('Test', ValueDate())
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from login.act import *     # Token bot
-from game import *
+from game import send_bot, game_117
 
 
 @bot.message_handler(commands=['start', 'help', 'game_117'])
-def send_welcome(message):    
+def send_welcome(message):  
     match message.text:
         case '/start':
             text_out = 'Добро пожаловать, ' + message.from_user.username
@@ -15,50 +15,16 @@ def send_welcome(message):
                 3. /game_117 - игра с конфетками.'
             send_bot(message, text_out)    
         case '/game_117':  
-            bot.register_next_step_handler(message,game_117)          
-        #     textov = f'Я, предлагаю Вам сыграть в игру:\n \
-        #         В игре у игроков есть кучка, в ней 117 конфет. \n \
-        #         Два игрока поочереди достают из кучки конфетки, \n \
-        #         но не более 28 конфет за один ход. \n \
-        #         Выигрывает тот игрок, чей ход окажется последним. \n \
-        #         Он и забирает все конфеты себе.'  
-        #     send_bot(message, textov)
-        #     send_bot(message, 'Играем да/нет')   
-             
-            # bot.register_next_step_handler(message, start_game)  
+            # bot.register_next_step_handler(message,game_117)
+            game_117(message)         
+        
+ 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    send_bot = lambda x: bot.send_message(message.chat.id, x)
+    
+    send_bot('Для ознакомления с функциями бота наберите /help')    
 
-""" Далее код самой игры 
-def start_game(message) -> str:
-    # send_bot = lambda x: bot.send_message(message.chat.id, x)
-    if message.text == 'да':    
-        log_win('Начало игры', ValueDate())
-        textov = f'Отлично! Сейчас мы определимся, кто будет ходить первым. \n \
-                Игрок 1 - это собственно Вы сами \n \
-                Игрок 2 - это я, бот Firsttest_bot.'    
-        send_bot(message, textov)
-        gamer = cur_gamer(0)
-        send_bot(message, f'Итак, первым ходит Игрок {gamer}')
-        run_game(gamer)
-        # bot.register_next_step_handler(message, choose_game) 
-        msg = ''
-    else:  
-        send_bot(message, "Ну, и ладно!!!")
-    
-def send_bot(message, in_text:str):
-    bot.send_message(message.chat.id, in_text)
-
-"""
-##### 
-# @bot.message_handler(func=lambda m: True)
-# def echo_all_1(message):
-#     send_bot = lambda x: bot.send_message(message.chat.id, x)
-    
-#     send_bot('Next')
-    
-# @bot.message_handler(func=lambda m: True)
-# def echo_game(message):
-#     send_bot = lambda x: bot.send_message(message.chat.id, x)
-#     send_bot('Next')
 bot.infinity_polling()
 
 
