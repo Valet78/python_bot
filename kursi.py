216674708ea5d42c16fbc0fbd3e476607040102e
Valet78 <valet78@mail.ru>
2022-12-26 19:34:37 +0300

    Начало попытки добаки курсов валют

---
+++ kursi.py
@@ -0,0 +1,9 @@
+from login.act import *     # Token bot
+
+
+
+URL = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=26/12/2022'
+
+
+if __name__=="__main__":
+    kk=0
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from login.act import *     # Token bot
 from game import send_bot, game_117
+from kursi import *
+
+def kurs_topic():
+    
+
+    ff=90
 
 
-@bot.message_handler(commands=['start', 'help', 'game_117'])
+
+@bot.message_handler(commands=['start', 'help', 'game_117', 'kurs'])
 def send_welcome(message):  
     match message.text:
         case '/start':
@@ -12,18 +19,31 @@ def send_welcome(message):
             text_out = 'В настоящее время Вы можете воспользоваться следующими командами: \n \
                 1. /start - запуск бота\n \
                 2. /help - собственно вызов этого сообщения\n \
-                3. /game_117 - игра с конфетками.'
+                3. /kurs - курс валют \n \
+                4. /game_117 - игра с конфетками \n'
             send_bot(message, text_out)    
-        case '/game_117':  
-            # bot.register_next_step_handler(message,game_117)
-            game_117(message)         
+        case '/game_117':             
+            game_117(message)    
+        case '/kurs':
+            bot.register_next_step_handler(message,kurs_topic)
+                 
         
- 
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    send_bot = lambda x: bot.send_message(message.chat.id, x)
+    # send_bot = lambda x: bot.send_message(message.chat.id, x)
     
-    send_bot('Для ознакомления с функциями бота наберите /help')    
+    send_bot(message,'Для ознакомления с функциями бота наберите /help')
+
+
+
+
+
+
+
+
+
+
 
 bot.infinity_polling()
 
